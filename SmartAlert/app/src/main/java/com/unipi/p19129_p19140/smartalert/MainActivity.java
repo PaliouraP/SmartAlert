ecbaab41e37f97ee6a7149606f14ad54bfe5a67d
PaliouraP <61277841+PaliouraP@users.noreply.github.com>
2023-01-28 23:40:21 +0200

    login logout register with firebase
    this is still under construction

--- SmartAlert/app/src/main/java/com/unipi/p19129_p19140/smartalert/MainActivity.java
+++ SmartAlert/app/src/main/java/com/unipi/p19129_p19140/smartalert/MainActivity.java
@@ -2,44 +2,40 @@ package com.unipi.p19129_p19140.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button register;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        TextView tv = findViewById(R.id.TextView1);
-        myRef.setValue("Hello, World! This is a new message");
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                tv.setText(value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("MainActivity", "Failed to read value.", error.toException());
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+
     }
 }
