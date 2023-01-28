712fdc32d5b25311c12427ef9fb8543ee685e325
PaliouraP <61277841+PaliouraP@users.noreply.github.com>
2023-01-28 04:33:56 +0200

    Firebase: First steps

--- SmartAlert/app/src/main/java/com/unipi/p19129_p19140/smartalert/MainActivity.java
+++ SmartAlert/app/src/main/java/com/unipi/p19129_p19140/smartalert/MainActivity.java
@@ -3,6 +3,14 @@ package com.unipi.p19129_p19140.smartalert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        TextView tv = findViewById(R.id.TextView1);
+        myRef.setValue("Hello, World! This is a message from far beyond.");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                tv.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("MainActivity", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
